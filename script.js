e346c39892dcd215489bd087b6afe63d7b325bfc
Boimah <boimah600@gmail.com>
2023-11-09 23:59:59 +0200

    added code to script.js and make some paragraph changes

---
+++ script.js
@@ -0,0 +1,56 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbyAKdmAcqc61z99nus9OTd26f4J6lNLfywG9_uFsJq33yuguv2aWubaWYFBQeHupObCxQ/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
+
+var topNav = document.getElementById("topNav"),
+  stop = topNav.offsetTop,
+  docBody =
+    document.documentElement || document.body.parentNode || document.body,
+  hasOffset = window.pageYOffset !== undefined,
+  scrollTop;
+
+window.onscroll = function (e) {
+  scrollTop = hasOffset ? window.pageYOffset : docBody.scrollTop;
+  if (scrollTop >= stop) {
+    topNav.className = "sticky";
+  } else {
+    topNav.className = "";
+  }
+};
