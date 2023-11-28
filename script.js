5f93230ca298332a75dc82467b74e5375ae3c736
Boimah <boimah600@gmail.com>
2023-11-28 22:59:43 +0200

    Change to all file

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
+
+// Function to close the sidebar when clicking away
+document.addEventListener("click", function (event) {
+  var sidebar = document.getElementById("nav");
+  var targetElement = event.target; // clicked element
+
+  do {
+    if (targetElement == sidebar) {
+      // Clicked inside the sidebar
+      return;
+    }
+    // Move up the DOM
+    targetElement = targetElement.parentNode;
+  } while (targetElement);
+
+  // Clicked outside the sidebar, so hide it
+  hideSidebar();
+});
+
+// Function to close the sidebar when clicking on an in-page anchor link
+document.querySelectorAll('#nav a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // Scroll to the target
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+
+    // Hide the sidebar
+    hideSidebar();
+  });
+});
+
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+}
+
 function opentab(tabname) {
   for (tablink of tablinks) {
     tablink.classList.remove("active-link");
@@ -10,15 +54,6 @@ function opentab(tabname) {
 
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
-}
-
-var sidemenu = document.getElementById("sidemenu");
-function openmenu() {
-  sidemenu.style.right = "0";
-}
-
-function closemenu() {
-  sidemenu.style.right = "-200px";
 }
 
 const scriptURL =
@@ -39,8 +74,8 @@ form.addEventListener("submit", (e) => {
     .catch((error) => console.error("Error!", error.message));
 });
 
-var topNav = document.getElementById("topNav"),
-  stop = topNav.offsetTop,
+var nav = document.getElementById("nav"),
+  stop = nav.offsetTop,
   docBody =
     document.documentElement || document.body.parentNode || document.body,
   hasOffset = window.pageYOffset !== undefined,
@@ -49,8 +84,8 @@ var topNav = document.getElementById("topNav"),
 window.onscroll = function (e) {
   scrollTop = hasOffset ? window.pageYOffset : docBody.scrollTop;
   if (scrollTop >= stop) {
-    topNav.className = "sticky";
+    nav.className = "sticky";
   } else {
-    topNav.className = "";
+    nav.className = "";
   }
 };
